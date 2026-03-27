ec1b73c66d3e0adbfcee9720702fb92dab8329e6
AsyncKeel <asynckeel@gmail.com>
2026-03-27 16:21:27 +0300

    Create test utility functions in tests/utils.py

---
+++ tests/utils.py
@@ -0,0 +1,22 @@
+# Test Utility Functions
+
+# This file contains utility functions and helpers for testing.
+
+def assert_equal(a, b):
+    """Asserts that two values are equal."""
+    assert a == b, f'Expected {a} to be equal to {b}'
+
+
+def assert_raises(exception_type, func, *args, **kwargs):
+    """Asserts that a specific exception is raised when calling a function."""
+    try:
+        func(*args, **kwargs)
+    except exception_type:
+        return
+    except Exception as e:
+        raise AssertionError(f'Expected {exception_type} but got {type(e).__name__}') from e
+    else:
+        raise AssertionError(f'Expected {exception_type} but no exception was raised')
+
+
+# Add more utility functions and helpers as needed.
