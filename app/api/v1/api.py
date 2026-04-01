423b1984399a86aebeeecf690e6786567238ab58
AsyncKeel <asynckeel@gmail.com>
2026-04-01 14:01:01 +0300

    Reorganize import statements for clarity

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth, users, organizations, organization_members, api_keys
+from app.api.v1.endpoints import (
+    api_keys,
+    auth,
+    organization_members,
+    organizations,
+    users,
+)
 
 api_router = APIRouter(prefix="/api/v1")
 
