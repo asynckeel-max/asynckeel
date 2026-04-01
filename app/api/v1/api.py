2d54aa0605094cd76bce178011fe528678f3d493
AsyncKeel <asynckeel@gmail.com>
2026-04-01 13:56:06 +0300

    Add new endpoints to API router

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth, users
+from app.api.v1.endpoints import auth, users, organizations, organization_members, api_keys
 
 api_router = APIRouter(prefix="/api/v1")
-api_router.include_router(users.router)
+
 api_router.include_router(auth.router)
+api_router.include_router(users.router)
+api_router.include_router(organizations.router)
+api_router.include_router(organization_members.router)
+api_router.include_router(api_keys.router)
